b148b01695a3c763f630a12ad2ec8290d027089c
gaborszita <gaborszita88@gmail.com>
2020-12-23 19:50:04 +0100

    add license

--- mainclient.cpp
+++ mainclient.cpp
@@ -1,3 +1,21 @@
+/* 
+* Copyright (C) 2020 Gabor Szita
+* 
+* This file is part of DragonTCP.
+*
+* DragonTCP is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as published by
+* the Free Software Foundation.
+*
+* DragonTCP is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with lidar project.  If not, see https://www.gnu.org/licenses/ 
+*/ 
+
 #include <iostream>
 #include "tcp.h"
 
