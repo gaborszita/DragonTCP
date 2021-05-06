cb84c2334acb06716854651e21a2eda9322c7a17
Gabor Szita <gaborszita88@gmail.com>
2021-05-06 09:55:03 +0200

    Find boost dependency

--- cmake/DragonTCPConfig.cmake
+++ cmake/DragonTCPConfig.cmake
@@ -2,5 +2,6 @@ include(CMakeFindDependencyMacro)
 
 set(THREADS_PREFER_PTHREAD_FLAG ON)
 find_dependency(Threads)
+find_dependency(Boost 1.66 COMPONENTS system)
 
 include("${CMAKE_CURRENT_LIST_DIR}/DragonTCPTargets.cmake")
